ab97c9f8daed4caced7604dad3070b7894361147
KSM <sminggo5@naver.com>
2020-08-05 13:55:37 +0900

    Add basic settings

---
+++ backend/unithis/src/main/java/com/unithis/UnithisApplication.java
@@ -0,0 +1,13 @@
+package com.unithis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnithisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnithisApplication.class, args);
+	}
+
+}
---
+++ backend/unithis/src/test/java/com/unithis/UnithisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unithis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnithisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
